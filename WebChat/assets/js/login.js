60e31b048dd46819a075dc2089f9199aefa0afde
Ramon <Ramon@RAMONIX>
2016-11-16 20:27:20 -0300

    Nuevo Login

---
+++ WebChat/assets/js/login.js
@@ -0,0 +1,24 @@
+﻿function validate() {
+    var uname = document.getElementById("uname");
+    if (uname === "" || uname === null) {        
+        return false;
+    }
+    return true;
+}
+
+
+$('#form').on('submit', function (e) {
+    var uname = $('#uname');
+
+    // Check if there is an entered value
+    if (!uname.val()) {
+        // Add errors highlight
+        uname.closest('.form-group').removeClass('has-warning').addClass('has-error');
+
+        // Stop submission of the form
+        e.preventDefault();
+    } else {
+        // Remove the errors highlight
+        uname.closest('.form-group').removeClass('has-warning').addClass('has-success');
+    }
+});
--- WebChat/assets/js/signalr.js
+++ WebChat/assets/js/signalr.js
@@ -2,116 +2,116 @@
 
     // Declare a proxy to reference the hub. 
     var chat = $.connection.chatHub;
-    var user = prompt("Nombre de usuario");
+    var user = document.getElementById("uname").value;
 
-    // Create a function that the hub can call to broadcast messages.
-    chat.client.sentMessage = function (name, message, time) {
-        // Codifica nombre y mensajes con HTML 
-        var encodedName = $('<div />').text(name).html();
-        var encodedMsg = $('<div />').text(message).html();
-        var encodedTime = $('<div />').text(time).html();
-        // Añade mensaje a la pagina
-        $('#discussion').append('<li class="i">' +
-            '<div class="head enviado">' +
-                '<span class="name">' + encodedName + '  </span>' +
-                '<span class="time">' + time + '</span>' +
-            '</div>' +
-            '<div class="enviado message">' + encodedMsg + '</div>' +
-        '</li>');
+        // Create a function that the hub can call to broadcast messages.
+        chat.client.sentMessage = function (name, message, time) {
+            // Codifica nombre y mensajes con HTML 
+            var encodedName = $('<div />').text(name).html();
+            var encodedMsg = $('<div />').text(message).html();
+            var encodedTime = $('<div />').text(time).html();
+            // Añade mensaje a la pagina
+            $('#discussion').append('<li class="i">' +
+                '<div class="head enviado">' +
+                    '<span class="name">' + encodedName + '  </span>' +
+                    '<span class="time">' + time + '</span>' +
+                '</div>' +
+                '<div class="enviado message">' + encodedMsg + '</div>' +
+            '</li>');
 
-        autoscroll();
-    };
+            autoscroll();
+        };
 
-    // Create a function that the hub can call to broadcast messages.
-    chat.client.receivedMessage = function (name, message, time) {
-        // Codifica nombre y mensajes con HTML 
-        var encodedName = $('<div />').text(name).html();
-        var encodedMsg = $('<div />').text(message).html();
-        var encodedTime = $('<div />').text(time).html();
-        // Añade mensaje a la pagina
-        $('#discussion').append('<li class="i">' +
-            '<div class="head recibido">' +
-                '<span class="name">' + encodedName + '  </span>' +
-                '<span class="time">' + time + '</span>' +
-            '</div>' +
-            '<div class="recibido message">' + encodedMsg + '</div>' +
-        '</li>');
+        // Create a function that the hub can call to broadcast messages.
+        chat.client.receivedMessage = function (name, message, time) {
+            // Codifica nombre y mensajes con HTML 
+            var encodedName = $('<div />').text(name).html();
+            var encodedMsg = $('<div />').text(message).html();
+            var encodedTime = $('<div />').text(time).html();
+            // Añade mensaje a la pagina
+            $('#discussion').append('<li class="i">' +
+                '<div class="head recibido">' +
+                    '<span class="name">' + encodedName + '  </span>' +
+                    '<span class="time">' + time + '</span>' +
+                '</div>' +
+                '<div class="recibido message">' + encodedMsg + '</div>' +
+            '</li>');
 
-        autoscroll();
-    };
+            autoscroll();
+        };
 
-    chat.client.onNewUserConnected = function (username) {
-        $('#discussion').append('<strong>Usuario ' + username
-            + ' conectado.</strong><hr />');
-        autoscroll();
-        var encodedName = $('<div />').text(username).html();
-
-        $('#list-users').append('<li>' +
-            '<img width="50" height="50" src="assets/images/usuario.jpg">' +
-            '<div class="info">' +
-            '<div class="user"><a href="javascript:register_popup(\'' + username + ' \', \'' + username + '\');">' + encodedName + '</a></div>' +
-                '<div class="status on"> online</div>' +
-                '</div></li>');
-    }
-
-    chat.client.onConnected = function (name) {
-        $.each(name, function (index, usuario) {
-            var encodedName = $('<div />').text(usuario.username).html();
+        chat.client.onNewUserConnected = function (username) {
+            $('#discussion').append('<strong>Usuario ' + username
+                + ' conectado.</strong><hr />');
+            autoscroll();
+            var encodedName = $('<div />').text(username).html();
 
             $('#list-users').append('<li>' +
                 '<img width="50" height="50" src="assets/images/usuario.jpg">' +
                 '<div class="info">' +
-                    '<div class="user"><a href="javascript:register_popup(\'' + usuario.username + ' \', \'' + usuario.username + '\');">' + encodedName + '</a></div>' +
+                '<div class="user"><a href="javascript:register_popup(\'' + username + ' \', \'' + username + '\');">' + encodedName + '</a></div>' +
                     '<div class="status on"> online</div>' +
                     '</div></li>');
-        });
-    }
+        }
 
-    chat.client.onUserDisconnected = function (username) {
-        $('#list-users li').each(function () {
-            if ($(this).find('.user').text() == username)
-                $(this).remove();
-        });
+        chat.client.onConnected = function (name) {
+            $.each(name, function (index, usuario) {
+                var encodedName = $('<div />').text(usuario.username).html();
 
-        $('#discussion').append('<strong>Usuario ' + username
-            + ' desconectado.</strong><hr />');
-        autoscroll();
-        autoscroll();
-    }
+                $('#list-users').append('<li>' +
+                    '<img width="50" height="50" src="assets/images/usuario.jpg">' +
+                    '<div class="info">' +
+                        '<div class="user"><a href="javascript:register_popup(\'' + usuario.username + ' \', \'' + usuario.username + '\');">' + encodedName + '</a></div>' +
+                        '<div class="status on"> online</div>' +
+                        '</div></li>');
+            });
+        }
 
-    chat.client.sentPrivateMessage = function (toUserId, fromUser, message) {
+        chat.client.onUserDisconnected = function (username) {
+            $('#list-users li').each(function () {
+                if ($(this).find('.user').text() == username)
+                    $(this).remove();
+            });
 
-    }
+            $('#discussion').append('<strong>Usuario ' + username
+                + ' desconectado.</strong><hr />');
+            autoscroll();
+            autoscroll();
+        }
 
-    chat.client.receivedPrivateMessage = function (fromUser, message) {
+        chat.client.sentPrivateMessage = function (toUserId, fromUser, message) {
 
-    }
+        }
 
-    // Get the user name and store it to prepend to messages.
-    $('#displayname').val(user);
+        chat.client.receivedPrivateMessage = function (fromUser, message) {
 
-    // Set initial focus to message input box.  
-    $('#message').focus();
-    // Start the connection.
-    $.connection.hub.start().done(function () {
-        chat.server.connect(user);
-        var message = document.getElementById("message");
-        message.addEventListener("keydown", function (e) {
-            if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-                e.preventDefault();
+        }
+
+        // Get the user name and store it to prepend to messages.
+        $('#displayname').val(user);
+
+        // Set initial focus to message input box.  
+        $('#message').focus();
+        // Start the connection.
+        $.connection.hub.start().done(function () {
+            chat.server.connect(user);
+            var message = document.getElementById("message");
+            message.addEventListener("keydown", function (e) {
+                if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+                    e.preventDefault();
+                    // Call the Send method on the hub. 
+                    chat.server.send($('#displayname').val(), $('#message').val());
+                    // Clear text box and reset focus for next comment. 
+                    $('#message').val('').focus();
+                    return false;
+                }
+            });
+
+            $('#sendmessage').click(function () {
                 // Call the Send method on the hub. 
                 chat.server.send($('#displayname').val(), $('#message').val());
                 // Clear text box and reset focus for next comment. 
                 $('#message').val('').focus();
-                return false;
-            }
-        });
-
-        $('#sendmessage').click(function () {
-            // Call the Send method on the hub. 
-            chat.server.send($('#displayname').val(), $('#message').val());
-            // Clear text box and reset focus for next comment. 
-            $('#message').val('').focus();
-        });
-    });
+            });
+        });    
 });
